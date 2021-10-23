ed1dbb5987b3de6c3c477e5e1ff5967828ad69f4
Cockamamie <bugrymenko@yandex.ru>
2021-10-24 03:30:40 +0500

    Changed args

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import argparse
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Take math expression derivative')
-    parser.add_argument('-e', '--expression', required=True,
-                        help='Differentiate given expression')
+    parser.add_argument('-e', '--expression', required=True, nargs='*',
+                        help='Differentiate given expression.'
+                             '\nTo write degree in windows use double "^".'
+                             '\nTo use Euler\'s number enter "e" in any case.'
+                             '\nTo use Pi enter "pi" in any case.')
     return parser
 
 
 def main():
     parser = get_args_parser()
     args = parser.parse_args()
-    root = parse(args.expression)
-    print(differentiate(root))
+    for expression in args.expression:
+        root = parse(expression)
+        print(differentiate(root))
 
 
 if __name__ == '__main__':
