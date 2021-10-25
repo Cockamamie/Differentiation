6918e9cecf952b1927a78680820613f24cf34dd7
Cockamamie <bugrymenko@yandex.ru>
2021-10-26 03:52:29 +0500

    Added list of supported expression elements

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from expression_parser import parse
 from differentiation import differentiate
 import argparse
 
+SUPPORTED = ['Integer numbers, x variable, +, -, *, /, ^, sin, cos, tan, cot']
+
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Take math expression derivative')
@@ -10,12 +12,16 @@ def get_args_parser():
                              '\nTo write degree in windows use double "^".'
                              '\nTo use Euler\'s number enter "e" in any case.'
                              '\nTo use Pi enter "pi" in any case.')
+    parser.add_argument('-s', '--supported', action='store_true',
+                        help='Get list of supported number types, variables, operations and functions')
     return parser
 
 
 def main():
     parser = get_args_parser()
     args = parser.parse_args()
+    if args.supported:
+        print(SUPPORTED)
     for expression in args.expression:
         root = parse(expression)
         print(differentiate(root))
